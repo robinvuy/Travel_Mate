fecb5aee8068b1b40dea9e7e7b3e8b9fc3d90bde
Robin Vuyyuru <robinvuy@gmail.com>
2023-10-10 22:39:05 +0100

    implemented form, currently trying to integrate fetch api get request with form input

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -4,17 +4,8 @@ const Clock = () => {
     const [info, setInfo] = useState([])
     const dateTime = info.datetime;
     const timeZone = info.timezone;
-    const URL = "http://worldtimeapi.org/api/timezone/Europe/London"
+    const URL = "http://worldtimeapi.org/api/timezone"
 
-    // useEffect(() => {
-    //     fetch(URL)
-    //         .then(response => response.json())
-    //         .then((json) => {
-    //             console.log(json);
-    //             setInfo(json);
-    //         })
-    // }, []);
-    
     useEffect(() => {
         const fetchData = async () => {
             const result = await fetch(URL)
@@ -25,14 +16,33 @@ const Clock = () => {
         fetchData();
     }, []);
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const payload = Object.fromEntries(formData);
+        console.log(payload.userInput); 
+        const resultTwo = fetch(`${URL}/${payload.userInput}`)
+        console.log(resultTwo);
+        const dataTwo = resultTwo.json()
+        // console.log(dataTwo);
+    // resultTwo();    
+    }
+    
+
     
     return (
         <div>
             <h1>hi</h1>
-            <label>
-                Enter Location: 
-                <input name ="userInput" defaultValue="hi"></input>
-            </label>
+                <form onSubmit={handleSubmit}> 
+                    <label>
+                        Enter Location: 
+                        <input name ="userInput" defaultValue="hi"></input>
+                        <button type="submit">Submit</button>
+                    </label>
+
+                </form>
+            
             
             <p>Chosen Timezone is: {timeZone && timeZone}</p>
             <p>time is: {dateTime && dateTime.slice(11,19)}</p>
