2be5774703e10e89b38742cf2cf3ec2a226db7f2
Robin Vuyyuru <robinvuy@gmail.com>
2023-10-13 18:22:58 +0100

    implemented successful api calls of data based on user input, need to clean up code now

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,30 +1,45 @@
 import React, { useState, useEffect } from "react";
 
 const Clock = () => {
-    const [info, setInfo] = useState([])
+    const [info, setInfo] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [submit, setSubmit] = useState(false)
     const dateTime = info.datetime;
     const timeZone = info.timezone;
     const URL = "http://worldtimeapi.org/api/timezone"
 
     useEffect(() => {
         const fetchData = async () => {
-            const result = await fetch(URL)
-            const data = await result.json()
-            console.log(data);
-            setInfo(data);
+            if (submit) {
+                const result = await fetch(`${URL}/${inputValue}`)
+                console.log(result)
+                const data = await result.json()
+                console.log(data);
+                setInfo(data);
+                setSubmit(false);
+            }
         }
-        fetchData();
-    }, []);
+            fetchData();
+    }, );
 
     function handleSubmit(e) {
         e.preventDefault();
-        const form = e.target;
-        const formData = new FormData(form);
-        const payload = Object.fromEntries(formData);
-        console.log(payload.userInput); 
-        const resultTwo = fetch(`${URL}/${payload.userInput}`)
-        console.log(resultTwo);
-        const dataTwo = resultTwo.json()
+        setSubmit(true)
+        // let inputValue = e.target.value
+        console.log(inputValue);
+
+        // fetchData(`${inputValue}`);
+        // setInputValue(inputValue);
+        // console.log(inputValue)
+        // fetchData();
+
+        // const form = e.target;
+        // const formData = new FormData(form);
+        // const payload = Object.fromEntries(formData);
+        // console.log(payload.userInput); 
+        // const resultTwo = fetch(`${URL}/${payload.userInput}`)
+        // console.log(resultTwo);
+        // const dataTwo = resultTwo.json()
         // console.log(dataTwo);
     // resultTwo();    
     }
@@ -34,10 +49,15 @@ const Clock = () => {
     return (
         <div>
             <h1>hi</h1>
-                <form onSubmit={handleSubmit}> 
+                <form onSubmit= {handleSubmit}> 
                     <label>
                         Enter Location: 
-                        <input name ="userInput" defaultValue="hi"></input>
+                        <input 
+                            type ="text" 
+                            required
+                            value = {inputValue} 
+                            onChange={(e) => setInputValue(e.target.value)}
+                        />
                         <button type="submit">Submit</button>
                     </label>
 
